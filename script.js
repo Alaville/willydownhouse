4eadba355ab59f0f345d12517c1f5e8819c59d1c
Alaville <72505209+Alaville@users.noreply.github.com>
2021-02-05 19:04:31 +0100

    ✌

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const input = document.querySelector(".input--1");
+const button = document.querySelector(".button__1");
+
+button.addEventListener("click", function (e) {
+  document
+    .getElementById("section--1")
+    .insertAdjacentHTML("beforeend", `<p>${input.value}</p>`);
+});
