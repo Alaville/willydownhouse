7796a89c487beb016a7a43f0b5a7746c3417003d
Alaville <72505209+Alaville@users.noreply.github.com>
2021-02-12 15:53:09 +0100

    ✅

--- script.js
+++ script.js
@@ -1,59 +1,83 @@
 "use strict";
 
-const input1 = document.querySelector(".input--1");
-const input2 = document.querySelector(".input--2");
-const input3 = document.querySelector(".input--3");
-const btn1 = document.querySelector(".button__1");
-const section1El = document.getElementById("section--1");
-const btn2 = document.querySelector(".button__2");
-const btn3 = document.querySelector(".button__3");
-const label = document.querySelector("label");
+// const input1 = document.querySelector(".input--1");
+// const input2 = document.querySelector(".input--2");
+// const input3 = document.querySelector(".input--3");
+// const btn1 = document.querySelector(".button__1");
+// const section1El = document.getElementById("section--1");
+// const btn2 = document.querySelector(".button__2");
+// const btn3 = document.querySelector(".button__3");
+// const label = document.querySelector("label");
 
-let html;
+const btnsAll = document.querySelectorAll(".button");
+let ones = 0;
+let twos = 0;
+let threes = 0;
+const randNums = [];
 
-class User {
-  constructor(name, address, email) {
-    this.name = name;
-    this.address = address;
-    this.email = email;
-  }
-}
+const newRandomNum = function () {
+  const random = Math.trunc(Math.random() * 3) + 1;
+  return random;
+};
 
-class App {
-  #users = [];
-  constructor() {
-    console.log("uusi appi käynnistyy");
-    /////////////////////////////// event listeners
-    btn1.addEventListener("click", this.addNewUser.bind(this));
+const btnPress = function (btn) {
+  let isPressed = false;
 
-    btn2.addEventListener("click", function () {
-      inputsAll.forEach((input) => {
-        //if (!input.value) return;
-        section1El.removeChild(document.querySelector(".span"));
-      });
-    });
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
 
-    btn3.addEventListener("click", this.funcNumAbdDisplayUsers.bind(this));
-  }
+    if (isPressed) return;
 
-  funcNumAbdDisplayUsers() {
-    let value = +label.textContent;
-    value += 1;
-    label.textContent = value;
-    console.log(this.#users);
-  }
+    //random num
+    let randomNum = newRandomNum();
+    randNums.push(randomNum);
 
-  addNewUser() {
-    const name = input1.value;
-    const address = input2.value;
-    const email = input3.value;
+    // max two times same random number
 
-    const user = new User(name, address, email);
+    // arvotut numerot
 
-    this.#users.push(user);
+    console.log(randNums);
+    console.log(e.target);
+    console.log(randomNum);
 
-    input1.value = input2.value = input3.value = "";
-  }
-}
+    e.target.src = `muisti${randomNum}.jpg`;
+    isPressed = true;
+  });
+};
 
-const app = new App();
+btnsAll.forEach((btn) => {
+  btnPress(btn);
+});
+
+/* btnsAll.forEach((btn) => {
+  btn.addEventListener("click", function (e) {
+    e.preventDefault();
+    let ones = 0;
+    let twos = 0;
+    let threes = 0;
+
+    const randomNum = Math.trunc(Math.random() * 3) + 1;
+    console.log(randomNum);
+
+    if (randomNum === 1 && ones < 2) {
+      ones++;
+      e.target.src = `muisti1.jpg`;
+    }
+    if (randomNum === 2 && twos < 2) {
+      twos++;
+      e.target.src = `muisti2.jpg`;
+    }
+    if (randomNum === 3 && threes < 2) {
+      threes++;
+      e.target.src = `muisti3.jpg`;
+    }
+
+    console.log(e.target);
+
+    // const img = e.target.children[0];
+    // console.log(img);
+
+    // e.target.children[0].src = "muisti1.jpg";
+  });
+});
+ */
